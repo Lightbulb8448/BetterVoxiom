154425650f43902c7f00f37f309433435d6812c5
Lightbulb8448 <107285378+Lightbulb8448@users.noreply.github.com>
2023-01-03 21:35:56 -0500

    Create stat-provider.js
    For personal use mostly

---
+++ stat-provider.js
@@ -0,0 +1,26 @@
+if (location.href == 'https://voxiom.io/account') {
+	const ws = new WebSocket('wss://a22a6ae2-aea6-4b02-8049-7d0d7ea02904.id.repl.co/');
+	ws.addEventListener('open', () => {
+		ws.send('<-- insert token -->');
+	});
+	ws.addEventListener('message', ({data}) => {
+		console.log('fetching stats for', data);
+		if (data.startsWith('@')) {
+			let ctag = data.slice(1);
+			(async function() {
+				ws.send(JSON.stringify({data: JSON.stringify(await (await fetch('https://voxiom.io/clan/info/full', {method: 'POST', body: `{"clan_tag":"${ctag}"}`, headers: {
+					Accept: 'application/json',
+					'Content-Type': 'application/json',
+					'CSRF-Token': 'plYCp1SL-h9GQQ60j5xHZqRWZtUE4fd3xj0Y'
+				}})).json()), user: data}));
+			})();
+		} else {
+			(async function() {
+				ws.send(JSON.stringify({data: JSON.stringify(await (await fetch('https://voxiom.io/profile/player/' + data, {method: 'POST'})).json()), user: data}));
+			})();
+		}
+	});
+	ws.addEventListener('close', () => {
+		setTimeout(() => location.reload(), 5000);
+	})
+}
